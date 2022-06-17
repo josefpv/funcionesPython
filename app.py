5513aced1119cf4ff952eec9346060317f13f2cd
josefpv <vivas.f13@gmail.com>
2022-06-17 02:00:54 +0000

    Update

--- app.py
+++ app.py
@@ -13,9 +13,28 @@ while True:
         opcionMenu = int(input("Seleccione una opcion del menu... "))
 
         if (opcionMenu) == 1:
-            funcs.numeroPrimo()
+
+            numero = int(input("Ingrese un numero entre el 1 y el 15: "))
+            if numero >= 1 and numero <= 15:
+
+                if funcs.numeroPrimo(numero):
+                    print("El numero es primo")
+                else:
+                    print("El numero no es primo")
+                
+                input("Presione cualquier tecla a continuar...")
+
+            else:
+                input("Numero fuera del rango, por favor presione cualquier tecla para intentar de nuevo")
+
         elif opcionMenu == 2:
-            funcs.factorial()
+            
+            numero = int(input("Ingrese un numero entre el 3 y el 10: "))
+            if numero >= 3 and numero <= 10:
+                print(f"Factorial de {numero} es {funcs.factorial(numero)}")
+
+            input("Presione cualquier tecla a continuar...")
+
         elif opcionMenu == 3:
             funcs.palindrome()
         elif opcionMenu == 4:
--- utils/funciones.py
+++ utils/funciones.py
@@ -1,4 +1,5 @@
 from os import system
+import math
 
 class Matematicas():
 
@@ -9,10 +10,19 @@ class Matematicas():
         return ["1. Numero primo.", "2. Factorial.", "3. Palindrome.", "4. Salir."]
 
     def numeroPrimo(self, numero):
-        print("Numero Primo")
+        
+        divisores = 0
+        for n in range(1, numero + 1):
+            if numero % n == 0:
+                divisores += 1
+
+        if divisores > 2:
+            return False
+        
+        return True
     
-    def factorial(self):
-        print("Factorial")
+    def factorial(self, numero):
+        return math.factorial(numero)
 
     def palindrome(self):
         print("Palindrome")
